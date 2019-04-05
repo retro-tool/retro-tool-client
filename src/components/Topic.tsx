c54b5f67e36c98843f25346212e0715cd41fc049
Rude <rude.ayelo@xing.com>
2019-04-05 11:37:18 +0200

    fix(CommentContainer): Revert a responsive fix

--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -11,7 +11,6 @@ export const TopicContainer = styled.div.attrs({
   m: [1, 2],
   minWidth: ["90%", "40%", null, null, 0]
 })<TopicContainerProps>`
-  position: relative;
   flex: 1;
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.16);
   background: white;
@@ -21,65 +20,20 @@ export const TopicContainer = styled.div.attrs({
 
   ${minWidth};
   ${space};
-
-  &::before {
-    content: "";
-    position: absolute;
-    z-index: 1;
-    height: 10px;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    border-bottom-right-radius: 4px;
-    border-bottom-left-radius: 4px;
-    background: linear-gradient(
-      to top,
-      white,
-      rgba(255, 255, 255, 0.8) 20%,
-      rgba(255, 255, 255, 0)
-    );
-  }
 `;
 
 type TitleProps = SpaceProps;
 const Title = styled.div.attrs<TitleProps>({
   p: [3, null, null, null, 4]
 })`
-  position: relative;
   display: flex;
   align-items: flex-start;
 
   ${space}
-
-  &::before {
-    content: "";
-    position: absolute;
-    z-index: 1;
-    height: 10px;
-    top: 100%;
-    left: 0;
-    right: 0;
-    background: linear-gradient(
-      to bottom,
-      white,
-      rgba(255, 255, 255, 0.8) 20%,
-      rgba(255, 255, 255, 0)
-    );
-  }
 `;
 
 const TitleIcon = styled.div`
   margin-top: 2px;
-`;
-
-interface ItemsContainerProps extends SpaceProps {}
-const ItemsContainer = styled.div.attrs({
-  pb: 2
-})<ItemsContainerProps>`
-  max-height: calc(100vh - 182px);
-  overflow: scroll;
-
-  ${space}
 `;
 
 interface TopicProps {
@@ -120,7 +74,7 @@ const Topic = ({
           <CreateActionItem />
         )}
       </Title>
-      <ItemsContainer>{children}</ItemsContainer>
+      {children}
     </TopicContainer>
   );
 };
