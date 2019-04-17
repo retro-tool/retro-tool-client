73bdfc38d7cb29aa8981a58847c721a116cc71f7
Rude <rude.ayelo@xing.com>
2019-04-17 16:26:26 +0200

    feat(ActionItem): Add the possibility to remove ActionItems

--- src/components/ActionItem.tsx
+++ src/components/ActionItem.tsx
@@ -2,6 +2,15 @@ import React, { ReactNode, useState } from "react";
 import styled from "styled-components/macro";
 import { space, SpaceProps } from "styled-system";
 import { Text } from "./";
+import { DeleteItem } from "./DeleteItemButton";
+
+const DeleteItemButton = styled(DeleteItem)`
+  display: none;
+  position: absolute;
+  top: 7px;
+  left: -13px;
+  transform: rotate(45deg);
+`;
 
 type ItemContainerProps = SpaceProps;
 const ItemContainer = styled.div.attrs<ItemContainerProps>({
@@ -10,11 +19,13 @@ const ItemContainer = styled.div.attrs<ItemContainerProps>({
   pt: [3, null, null, null, 3],
   pb: [3, null, null, null, 3]
 })`
-  display: flex;
-  align-items: flex-start;
   position: relative;
 
   ${space};
+
+  &:hover ${DeleteItemButton} {
+    display: block;
+  }
 `;
 
 type ItemProps = {
@@ -22,8 +33,9 @@ type ItemProps = {
   id: string;
   completed: boolean;
 };
-const Item = ({ children }: ItemProps) => (
+const Item = ({ children, id }: ItemProps) => (
   <ItemContainer>
+    <DeleteItemButton id={id} />
     <Text ml={2}>{children}</Text>
   </ItemContainer>
 );
